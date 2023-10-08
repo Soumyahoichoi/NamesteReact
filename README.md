6c47b25d3cfea4bcc9eeb2b306e1717a2813ceb7
soumya <soumya.chakraborty@hoichoi.tv>
2023-10-08 22:10:39 +0530

    Igniting Our App

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # NamesteReact
+
+# Parcel
+
+- Dev Build
+- Local Server
+- Hot Module Replacement
+- File Watching Algorithm
+- Caching - Faster Builds
+- Image Optimization
+- Minification
+- Bundling
+- Compress
+- Code Splitting
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling
+- Diagnostic
+- Error Handling
+- Tree Shaking
--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
 const parent = React.createElement('div', { id: 'parent' }, [
 	React.createElement('div', { id: 'child1' }, [
-		React.createElement('h1', { id: 'head' }, 'Hello World from React'),
+		React.createElement('h1', { id: 'head' }, 'This is Nameste React'),
 		React.createElement('h2', { id: 'para' }, 'This is a paragraph')
 	]),
 	React.createElement('div', { id: 'child2' }, [
